653336baae7191b551b93f5044f3948ea10f226c
Brian Wangora <brianwangora@gmail.com>
2022-06-03 11:59:14 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const employee = {
+    Bob: "1121-Acacia",
+    Steven: "234-Lincoln",
+    Jane: "342-Waverly",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = {...employee}
+    newemployee[key] = value
+    return newemployee
+};
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value
+    return employee
+};
+function deleteFromEmployeeByKey (employee, key) {
+    const employee1 = {...employee}
+    delete employee1[key];
+    return employee1
+};   
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+}
